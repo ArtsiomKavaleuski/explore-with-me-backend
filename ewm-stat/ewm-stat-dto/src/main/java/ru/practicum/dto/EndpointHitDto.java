5c619487b234fd022d556c1a4dd2d1d1d042d480
Koval <artemKS333@yandex.kz>
2025-03-07 19:42:55 +0300

    написал сервис статистики, проверил checkstyle

---
+++ ewm-stat/ewm-stat-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EndpointHitDto {
+    Long id;
+    private String app;
+    private String uri;
+    private String ip;
+    private LocalDateTime timestamp;
+}
